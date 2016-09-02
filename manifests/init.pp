a8e349b9221b8f0935035808c73e883be6c53845
a.ziethen <a.ziethen@ssh1.scalecommerce.rocks>
2016-09-02 14:01:38 +0200

    initial commit of skeleton

---
+++ manifests/init.pp
@@ -0,0 +1,41 @@
+# == Class: sc_supervisor
+#
+# This is a wrapper for ajcrowe-supervisord module to add some specific
+# funtionality for scalecommerce projects.
+#
+# === Parameters
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# Here you should define a list of variables that this module would require.
+#
+# [*sample_variable*]
+#   Explanation of how this variable affects the funtion of this class and if
+#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#   External Node Classifier as a comma separated list of hostnames." (Note,
+#   global variables should be avoided in favor of class parameters as
+#   of Puppet 2.6.)
+#
+# === Examples
+#
+#  class { 'dummy':
+#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#  }
+#
+# === Authors
+#
+# Andreas Ziethen <az@scale.sc>
+#
+# === Copyright
+#
+# Copyright 2016 ScaleCommerce GmbH, Berlin
+#
+class sc_supervisor (
+) {
+  
+
+}
