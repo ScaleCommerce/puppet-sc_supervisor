6d83c670753e23a40003c673f96aa31ba1bf7a7a
Andreas Ziethen <az@scale.sc>
2016-09-08 14:42:14 +0200

    add param for init scripts

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,26 +5,8 @@
 #
 # === Parameters
 #
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
-# === Examples
-#
-#  class { 'dummy':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
+# [*init_path*]
+#   path to supervisor init scripts
 #
 # === Authors
 #
@@ -35,7 +17,9 @@
 # Copyright 2016 ScaleCommerce GmbH, Berlin
 #
 class sc_supervisor (
+  $init_path        = '/etc/supervisor.init',
 ) {
-  
+
+  include supervisord
 
 }
