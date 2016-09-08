6bf31603a5e9eaf0976062fa0dc2eac13d42d072
Andreas Ziethen <az@scale.sc>
2016-09-08 18:47:08 +0200

    remove creation of supervisor.d directory

--- manifests/init.pp
+++ manifests/init.pp
@@ -26,28 +26,6 @@ class sc_supervisor (
     ensure => absent,
   }
 
-  $source_dir = '/etc/supervisor/conf.d'
-  $target_dir = '/etc/supervisor.d'
-
-  file { 'new_supervisor_conf_dir' :
-    path => $target_dir,
-    ensure => 'directory',
-    source => "file://${source_dir}",
-    recurse => true,
-    before => File[$source_dir],
-  }->
-  file { 'old_supervisor_conf_dir' :
-    path => $source_dir,
-    ensure => 'absent',
-    purge => true,
-    recurse => true,
-    force => true,
-  }->
-  file { $source_dir:
-    ensure => link,
-    target => $target_dir,
-  }
-
 
   file { $sc_supervisor::init_path:
     ensure => directory,
